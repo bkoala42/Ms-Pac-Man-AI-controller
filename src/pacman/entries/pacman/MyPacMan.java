f521480cc51ad587018c49001f3552a27620ce0a
JBFourierous <marco.carpentiero@gmail.com>
2020-06-10 12:20:23 +0200

    Added utility functions

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -1,8 +1,13 @@
 package pacman.entries.pacman;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import pacman.controllers.Controller;
+import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
+
 
 /*
  * This is the class you need to modify for your entry. In particular, you need to
@@ -18,5 +23,46 @@ public class MyPacMan extends Controller<MOVE>
 		//Place your game logic here to play the game as Ms Pac-Man
 		
 		return myMove;
+	}
+	
+	/**
+	 * Computes a weighted average of the shortest path distance of MsPacman from ghosts
+	 * @param  game game instance
+	 * @param  posMsPacman current index of MsPacman
+	 * @return weighted average distance from ghosts
+	 */
+	private float weightedGhostsDistance(Game game, int posMsPacman) {
+		ArrayList<Float> distances = new ArrayList<Float>(4);
+		for(GHOST ghost : GHOST.values()) {
+			distances.add((float)game.getShortestPathDistance(posMsPacman, game.getGhostCurrentNodeIndex(ghost)));
+		}
+		float maxDistance = Collections.max(distances);
+		float meanDistance = 0;
+		for(Float dist : distances) {
+			meanDistance += (dist/maxDistance)*dist;
+		}
+		return meanDistance;
+	}
+	
+	/**
+	 * Objective function (utility) for "conservative" strategy
+	 * @param  distances array containing, in order, average distance from ghosts, distance from nearest power pill, 
+	 *                   distance from nearest pill
+	 * @param  hypParam hyperparameters for the utility function
+	 * @return utility value
+	 */
+	private double utility0(float[] distances, int[] hypParam) {
+		return Math.pow(distances[0], hypParam[0]) + Math.pow(distances[1], hypParam[1]) + Math.pow(distances[2], -hypParam[2]);
+	}
+	
+	/**
+	 * Objective function (utility) for "hot pursuit" strategy
+	 * @param  distances array containing, in order, average distance from ghosts, distance from nearest power pill, 
+	 *                   distance from nearest pill
+	 * @param  hypParam hyperparameters for the utility function
+	 * @return utility value
+	 */
+	private double utility1(float[] distances, int[] hypParam) {
+		return hypParam[0]*distances[0] + Math.pow(distances[1], -hypParam[1]) - hypParam[2]*distances[2];
 	}
 }
